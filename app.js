ab17fc82d1ff10f57395023f478e50d32b2d16a9
Ryan Token <ryantoken13@icloud.com>
2019-05-07 21:01:57 -0500

    adopted proper REST url scheme (url.com/api/resource)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}))
 })
 
-app.get('/yttv', function (req, res) {
+app.get('/api/yttv', function (req, res) {
     //Scrape YouTube TV Channels
     axios.get('https://www.groundedreason.com/youtube-tv-channels/').then((response) => {
         //Load the web page source code into a cheerio instance
@@ -29,7 +29,7 @@ app.get('/yttv', function (req, res) {
     })
 })
 
-app.get('/hulu', function (req, res) {
+app.get('/api/hulu', function (req, res) {
     //Scrape Hulu Live TV Channels
     axios.get('https://www.groundedreason.com/tv-shows-hulu-worth-cost/').then((response) => {
         //Load the web page source code into a cheerio instance
@@ -49,7 +49,7 @@ app.get('/hulu', function (req, res) {
     })
 })
 
-app.get('/vue', function (req, res) {
+app.get('/api/vue', function (req, res) {
     //Scrape PlayStation Vue Access Bundle Channels
     axios.get('https://www.groundedreason.com/replace-cable-with-playstation-vue/').then((response) => {
         //Load the web page source code into a cheerio instance
